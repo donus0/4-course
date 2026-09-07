450f826c04c7d4b7044003355615e127d86b3781
donus0 <ekzozeta@gmail.com>
2026-09-07 22:55:39 +0300

    add cmake + russian lang

--- postfix/src/main.cpp
+++ postfix/src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "InfixToPostfix.hpp"
 #include "PostfixEval.hpp"
 
@@ -37,6 +41,11 @@ void handleEvaluate() {
 }
 
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     while (true) {
         printMenu();
         std::string choice;
